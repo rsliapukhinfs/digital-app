1ee64df09eefe92b9f99ca99de239032f7123850
rsliapukhinfs <rsliapukhinfs@gmail.com>
2023-08-25 19:28:22 +0300

    add layout

---
+++ comps/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>digital &copy; 2023. All rights reserved</div>
+    </footer>
+  );
+};
+
+export default Footer;
